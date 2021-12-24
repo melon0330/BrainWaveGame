03dd4372bb4b3c39890720e85481f1d3703608dc
sangjin <qotkdwls88@naver.com>
2021-12-24 23:51:49 +0900

    Updated by Sangjin

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    public bool isTouchBottom;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+       }
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if (isTouchBottom && h == 1)
+            h = 0;
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed*(-1))
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Assets/Sprites/Wizard - 2D Character/Demo/SimplePlayerController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+
+namespace ClearSky
+{
+    public class SimplePlayerController : MonoBehaviour
+    {
+        public float movePower = 10f;
+        public float jumpPower = 15f; //Set Gravity Scale in Rigidbody2D Component to 5
+
+        private Rigidbody2D rb;
+        private Animator anim;
+        Vector3 movement;
+        private int direction = 1;
+        bool isJumping = false;
+        private bool alive = true;
+        public SpriteRenderer renderer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+            
+            
+        }
+
+        private void Update()
+        {
+            Restart();
+            if (alive)
+            {
+                Hurt();
+                Die();
+                Attack();
+                Jump();
+                Run();
+
+            }
+            
+        }
+        void FixedUpdate()
+        {   if(rb.velocity.y < 0) {
+                Debug.DrawRay(rb.position, Vector3.down, new Color(0, 1, 0));
+
+                RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1, LayerMask.GetMask("Flat"));
+
+                if (rayHit.collider != null)
+                {
+                    if (rayHit.distance < 0.001f)
+                        anim.SetBool("isJump", false);
+                }
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            anim.SetBool("isJump", false);
+        }
+
+
+        void Run()
+        {
+            Vector3 moveVelocity = Vector3.zero;
+            anim.SetBool("isRun", false);
+
+
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                direction = -1;
+                moveVelocity = Vector3.left;
+
+                transform.localScale = new Vector3(direction, 0.5f, 0.5f);
+
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                direction = 1;
+                moveVelocity = Vector3.right;
+
+                transform.localScale = new Vector3(direction, 0.5f, 0.5f);
+
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            transform.position += moveVelocity * movePower * Time.deltaTime;
+        }
+        void Jump()
+        {
+            if ((Input.GetButtonDown("Jump") || Input.GetAxisRaw("Vertical") > 0)
+            && !anim.GetBool("isJump"))
+            {
+                isJumping = true;
+                anim.SetBool("isJump", true);
+            }
+            if (!isJumping)
+            {
+                return;
+            }
+
+            rb.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+            isJumping = false;
+        }
+        void Attack()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                anim.SetTrigger("attack");
+            }
+        }
+        void Hurt()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                anim.SetTrigger("hurt");
+                if (direction == 1)
+                    rb.AddForce(new Vector2(-5f, 1f), ForceMode2D.Impulse);
+                else
+                    rb.AddForce(new Vector2(5f, 1f), ForceMode2D.Impulse);
+            }
+        }
+        void Die()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                anim.SetTrigger("die");
+                alive = false;
+            }
+        }
+        void Restart()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                anim.SetTrigger("idle");
+                alive = true;
+            }
+        }
+    }
+}
